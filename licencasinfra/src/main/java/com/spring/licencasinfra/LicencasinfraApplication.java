9107274865944a633474ab3c04f9450e01b18e62
perrutbruno <bruno.muniz.perrut@gmail.com>
2020-09-24 23:12:57 -0300

    Adicao pasta src do projeto com springDataJPA, springLDAP e PostgresqlDriver

---
+++ licencasinfra/src/main/java/com/spring/licencasinfra/LicencasinfraApplication.java
@@ -0,0 +1,13 @@
+package com.spring.licencasinfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicencasinfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicencasinfraApplication.class, args);
+	}
+
+}
---
+++ licencasinfra/src/test/java/com/spring/licencasinfra/LicencasinfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.licencasinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicencasinfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
